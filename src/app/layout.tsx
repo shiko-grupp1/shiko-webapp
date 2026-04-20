65d8882b288b53c20ed9e0ff2ed28b5ec35ed426
THEOD0R1 <squirrelnub@gmail.com>
2026-04-20 14:58:25 +0200

    feat: added Archivo fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,29 @@
 import type { Metadata } from "next";
+import localFont from "next/font/local";
 import "./globals.css";
 
 export const metadata: Metadata = {
   title: "",
   description: "",
 };
+
+const archivo = localFont({
+  src: [
+    {
+      path: "./fonts/Archivo-VariableFont.woff2",
+      weight: "100 900",
+      style: "normal",
+    },
+    {
+      path: "./fonts/Archivo-Italic-VariableFont.woff2",
+      weight: "100 900",
+      style: "italic",
+    },
+  ],
+  variable: "--font-archivo",
+  fallback: ["system-ui", "arial", "sans-serif"],
+  display: "swap",
+});
 
 export default function RootLayout({
   children,
@@ -13,7 +32,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={archivo.className}>{children}</body>
     </html>
   );
 }
