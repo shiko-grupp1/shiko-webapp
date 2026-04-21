4553a841edbda0f799e43d5fc8208826f1abdf17
Lizza96 <ia-hugo@hotmail.com>
2026-04-21 14:24:20 +0200

    add component MenuListItem and _menuItem.scss.

---
+++ src/app/components/ui/MenuItem/MenuListItem.tsx
@@ -0,0 +1,26 @@
+type MenuItem = {
+  icon: React.ReactNode;
+  text: string;
+  isActive: boolean;
+  notifications?: number;
+};
+type MenuItemProps = {
+  menuItem: MenuItem;
+};
+
+export default function MenuListItem({ menuItem }: MenuItemProps) {
+  const itemClassName = `menu-item ${menuItem.isActive ? "active" : ""}`;
+  return (
+    <li className={itemClassName}>
+      <div className="left-column">
+        <span className="menu-icon">{menuItem.icon}</span>{" "}
+        <span className="menu-text">{menuItem.text}</span>
+      </div>
+      {menuItem.notifications ? (
+        <div className="notification">
+          <span>{menuItem.notifications}</span>
+        </div>
+      ) : null}
+    </li>
+  );
+}
