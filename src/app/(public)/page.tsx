19966a6361936720a6f6a301136aba98174e06e8
Lizza96 <ia-hugo@hotmail.com>
2026-04-21 14:34:48 +0200

    stage page.tsx

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,7 +1,20 @@
+import DashboardIcon from "../components/icons/dashboardIcon";
+import MenuItem from "../components/ui/MenuItem/MenuListItem";
+
 export default function Home() {
+  const menuItem = {
+    icon: <DashboardIcon />,
+    text: "Dashboard",
+    isActive: true,
+    notifications: 3,
+  };
+
   return (
     <>
       <h1 className="">HOME</h1>
+      <div className="test">
+        <MenuItem menuItem={menuItem} />
+      </div>
     </>
   );
 }
