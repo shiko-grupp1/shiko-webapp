04236ef87e2c313871f72922f53a66fb86726c76
Alicia Petersson <82518756+Lizza96@users.noreply.github.com>
2026-04-23 13:44:22 +0200

    Added Menu Link Item (#6)
    * add component MenuListItem and _menuItem.scss.
    
    * stage page.tsx
    
    * Revert "stage page.tsx", accidentally commited wrong file
    
    This reverts commit 19966a6361936720a6f6a301136aba98174e06e8.
    
    * fix: folder structure after merge
    
    * fix: wrong relative path to colors.scss in Button.module.scss
    
    * fix: added pointer cursor and changed font size to rem
    
    * fix: made list item a link
    
    * fix: change a-element to a NextJs Link-element
    
    * fix: changed from scss to vanilla css for MenuListItem
    
    * fix: corecctly using module.css file for MenuListItem component
    
    * fix: corrected opacity color variables
    
    * fix: accidentally added text css class
    
    * fix: changed to use typography class on menu list item text
    
    * refactor: changed to individual props intead of a combined prop for MenuListItem.
    
    * fix: added hover effect

---
+++ src/app/components/shared/MenuItem/MenuListItem.tsx
@@ -0,0 +1,29 @@
+import Link from "next/link";
+import styles from "./MenuListItem.module.css";
+
+type MenuItemProps = {
+  icon: React.ReactNode;
+  text: string;
+  href: string;
+  isActive: boolean;
+  notifications?: number;
+};
+
+export default function MenuListItem({ icon, text, href, isActive, notifications }: MenuItemProps) {
+  const itemClassName = `${styles["menu-item"]} ${isActive ? styles["active"] : ""}`;
+  return (
+    <li className={itemClassName}>
+      <Link className={styles["menu-item-content"]} href={href}>
+        <div className={styles["left-column"]}>
+          <span className={styles["menu-icon"]}>{icon}</span>{" "}
+          <span className={`${styles["menu-text"]} body-20`}>{text}</span>
+        </div>
+        {notifications ? (
+          <div className={styles.notification}>
+            <span>{notifications}</span>
+          </div>
+        ) : null}
+      </Link>
+    </li>
+  );
+}
