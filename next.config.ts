25cb9d87d51bf6fdff18b849fe3e9a47305794d8
THEOD0R1 <squirrelnub@gmail.com>
2026-04-28 13:15:39 +0200

    feat: removed unnecessary nextConfig.

--- next.config.ts
+++ next.config.ts
@@ -2,15 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "i.ibb.co",
-        pathname: "**",
-      },
-    ],
-  },
 };
 
 export default nextConfig;
