43df881432e75201c129d97797d208f77c87a598
Elias Alattar <elias.alattar@yh.nackademin.se>
2026-04-28 10:34:22 +0200

    removed testcode

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,19 +1,3 @@
-"use client";
-
-import { useState } from "react";
-import Toggle from "../components/shared/Toggle/Toggle";
-
 export default function Home() {
-  const [emailEnabled, setEmailEnabled] = useState(true);
-  const [smsEnabled, setSmsEnabled] = useState(false);
-
-  return (
-    <main style={{ padding: "40px" }}>
-      <div style={{ display: "flex", flexDirection: "column", gap: "12px" }}>
-        <Toggle checked={emailEnabled} onChange={setEmailEnabled} label="Email" />
-
-        <Toggle checked={smsEnabled} onChange={setSmsEnabled} label="SMS" />
-      </div>
-    </main>
-  );
+  return <main></main>;
 }
