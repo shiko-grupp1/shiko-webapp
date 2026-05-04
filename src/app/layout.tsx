8a81ac4a40298f37db8636665c14c53f36265077
osmanjeylani <osmanjeylani00@gmail.com>
2026-05-04 10:37:22 +0200

    removed sidebar from layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import { Archivo } from "next/font/google";
-import Sidebar from "./components/sidebar/SideBar";
 import "./globals.css";
 
 const archivo = Archivo({
@@ -17,18 +16,13 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
-      <body className={`${archivo.className} layout`}>
-        <div className="layout">
-          <div className="sidebar-wrapper">
-            <Sidebar />
-          </div>
-          <main>{children}</main>
-        </div>
+      <body className={`${archivo.className}`}>
+        {children}
       </body>
     </html>
   );
