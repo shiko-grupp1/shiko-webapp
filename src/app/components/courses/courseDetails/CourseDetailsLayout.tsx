4db92d7f5fcd3ccead0032f36776c7dd21c3e823
THEOD0R1 <squirrelnub@gmail.com>
2026-05-04 15:16:27 +0200

    feat: update CourseDetailsLayout styling and improve global styles

--- src/app/components/courses/courseDetails/CourseDetailsLayout.tsx
+++ src/app/components/courses/courseDetails/CourseDetailsLayout.tsx
@@ -1,14 +1,25 @@
 import Image from "next/image";
+import CalendarIcon from "../../icons/CalendarIcon";
 
 export default function CourseDetailsLayout() {
   return (
-    <section className="flex flex-col p-8 gap-8 bg-white rounded-lg">
-      <Image
-        alt="test"
-        src="https://filestoragesuper.blob.core.windows.net/images/25b8346e-463a-4e10-9ea6-c4d7655121b9.webp"
-        width={500}
-        height={300}
-      />
+    <section className="w-fit flex flex-col p-8 gap-8 bg-white rounded-[35px]">
+      <div className="overflow-hidden w-auto rounded-[35px]">
+        <Image
+          alt="test"
+          src="https://filestoragesuper.blob.core.windows.net/images/25b8346e-463a-4e10-9ea6-c4d7655121b9.webp"
+          width={1000}
+          height={700}
+        />
+      </div>
+      <h2 className="header-semi-40">Foundations of Digital Marketing</h2>
+      <section className="flex gap-4">
+        <CalendarIcon />
+        <CalendarIcon />
+        <CalendarIcon />
+        <CalendarIcon />
+        <CalendarIcon />
+      </section>
     </section>
   );
 }
