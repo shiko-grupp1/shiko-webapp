b7b9852b3ca6dbf1a81c2a109be2971c734ffb65
THEOD0R1 <squirrelnub@gmail.com>
2026-05-06 12:56:55 +0200

    feat: implement CourseDetailsLayout with navigation and overview components

--- src/app/components/courses/courseDetails/CourseDetailsLayout.tsx
+++ src/app/components/courses/courseDetails/CourseDetailsLayout.tsx
@@ -1,25 +1,68 @@
+"use client";
 import Image from "next/image";
-import CalendarIcon from "../../icons/CalendarIcon";
+import CourseDetailsNav from "./CourseDetailsNav";
+import { useState } from "react";
+import CourseOverview from "./CourseOverview";
 
-export default function CourseDetailsLayout() {
+type CourseDetailsLayoutProps = {
+  videoUrl?: string;
+  title?: string;
+  lessonAmount?: number;
+  duration?: string;
+  reviewData?: string; //Should be review data type
+};
+
+const componentMap = {
+  Overview: CourseOverview,
+  FAQ: CourseOverview,
+  Reviews: CourseOverview,
+  Instructor: CourseOverview,
+};
+
+export default function CourseDetailsLayout({
+  videoUrl,
+  title,
+  lessonAmount,
+  duration,
+  reviewData,
+}: CourseDetailsLayoutProps) {
+  const [activeComp, setActiveComp] = useState("Overview");
+  const handleNavClick = (compName?: string) => {
+    if (compName) setActiveComp(compName);
+  };
+
+  const SelectedComponent = componentMap[activeComp as keyof typeof componentMap] || null;
+
+  const lessonInfoStyle =
+    "flex items-center gap-5 after:content-[''] after:bg-light-white after:inline-block after:w-px after:h-2.5";
+
   return (
-    <section className="w-fit flex flex-col p-8 gap-8 bg-white rounded-[35px]">
+    <section className="w-fit min-h-full max-w-175 flex flex-col p-8 gap-8 bg-white rounded-[35px]">
       <div className="overflow-hidden w-auto rounded-[35px]">
         <Image
           alt="test"
           src="https://filestoragesuper.blob.core.windows.net/images/25b8346e-463a-4e10-9ea6-c4d7655121b9.webp"
           width={1000}
           height={700}
+          className="w-auto h-auto object-cover"
+          loading="eager"
         />
       </div>
-      <h2 className="header-semi-40">Foundations of Digital Marketing</h2>
-      <section className="flex gap-4">
-        <CalendarIcon />
-        <CalendarIcon />
-        <CalendarIcon />
-        <CalendarIcon />
-        <CalendarIcon />
-      </section>
+      {title && <h2 className="header-semi-40">{title}</h2>}
+      <ul className="flex gap-5 ">
+        <li className={lessonInfoStyle}>{lessonAmount} lessons</li>
+        <li className={lessonInfoStyle}>{duration} duration</li>
+        <li>{reviewData} reviews</li>
+      </ul>
+      <CourseDetailsNav
+        navItems={[
+          { label: "Overview", compName: "Overview", onClick: handleNavClick },
+          { label: "FAQ", compName: "FAQ", onClick: handleNavClick },
+          { label: "Reviews", compName: "Reviews", onClick: handleNavClick },
+          { label: "Instructor", compName: "Instructor", onClick: handleNavClick },
+        ]}
+      />
+      <section>{SelectedComponent && <SelectedComponent />}</section>
     </section>
   );
 }
