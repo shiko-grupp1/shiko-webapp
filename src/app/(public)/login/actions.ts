04a2c3ca407ad8d987506ef832525af7709a72db
Lizza96 <ia-hugo@hotmail.com>
2026-05-20 14:30:46 +0200

    fix: removed test login form

--- src/app/(public)/login/actions.ts
+++
@@ -1,7 +0,0 @@
-"use server";
-
-import { emailIsVerified } from "@/app/lib/helper/email/emailIsVerified";
-
-export async function emailIsVerifiedAction(email: string) {
-  return await emailIsVerified(email);
-}
